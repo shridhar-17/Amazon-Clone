e37468dbedfbd330a6687b3640a039997201879d
shridhar-17 <shridhargawande2002@gmail.com>
2024-10-22 19:47:04 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,20 @@
+console.log("Welcome to Java Script Course");
+age=24;
+name="Shridhar"
+console.log(name);
+
+const student={
+ fulllName:"Rahul",
+ age:20,
+ rollNo:1234,
+ isPass:true
+};
+student["age"]=40
+console.log(student.age)
+
+let gameNo=25;
+// let userNo=prompt("Guess  the number");
+// while(userNo!=gameNo){
+//     userNo=prompt("wrong Number,please guess again:");
+// }
+// alert("Congratulation you chose correct number");
